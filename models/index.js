5d020964e7a103fefb4bb270ece4b9b7df908733
Kyro <mariogallardo1331@yahoo.com>
2022-02-07 19:17:04 -0600

    creating comment routes

--- models/index.js
+++ models/index.js
@@ -4,42 +4,60 @@ const Post = require('./Post');
 
 const Vote = require('./Vote');
 
-// Create associations
+const Comment = require('./Comment');
+
+// create associations
 User.hasMany(Post, {
     foreignKey: 'user_id'
-});
-
-// Reverse association
-Post.belongsTo(User, {
-    foreignKey: 'user_id',
-});
-
-User.belongsToMany(Post, {
+  });
+  
+  Post.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  User.belongsToMany(Post, {
     through: Vote,
-    as: 'voted_post',
+    as: 'voted_posts',
     foreignKey: 'user_id'
-});
-
-Post.belongsToMany(User, {
+  });
+  
+  Post.belongsToMany(User, {
     through: Vote,
-    as: 'voted_post',
+    as: 'voted_posts',
     foreignKey: 'post_id'
-});
-
-Vote.belongsTo(User, {
+  });
+  
+  Vote.belongsTo(User, {
     foreignKey: 'user_id'
-});
-
-Vote.belongsTo(Post, {
+  });
+  
+  Vote.belongsTo(Post, {
     foreignKey: 'post_id'
-});
-
-User.hasMany(Vote, {
+  });
+  
+  User.hasMany(Vote, {
     foreignKey: 'user_id'
-});
-
-Post.hasMany(Vote, {
+  });
+  
+  Post.hasMany(Vote, {
     foreignKey: 'post_id'
-});
-
-module.exports = { User, Post, Vote };
+  });
+  
+  Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+  
+  module.exports = { User, Post, Vote, Comment };
+  
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
+const commentRoutes = require('./api/comment-routes');
+
+router.use('/comments', commentRoutes);
+
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
