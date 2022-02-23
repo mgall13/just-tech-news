45714358934cbcf0b731dfcc9052e9be26880ef5
Kyro <mariogallardo1331@yahoo.com>
2022-02-22 19:54:04 -0600

    reworking Comment.create route in comment-routes.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
-const withAuth = require('../../')
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
   Comment.findAll()
@@ -11,22 +11,20 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
-  // check the session
-  if (req.session) {
-    Comment.create({
-      comment_text: req.body.comment_text,
-      post: req.body.post_id,
-      // use the id from the session
-      user_id: req.session.user_id
-    })
+router.post('/', withAuth, (req, res) => {
+  // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.session.user_id,
+    post_id: req.body.post_id
+  })
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
       console.log(err);
       res.status(400).json(err);
     });
-  }
 });
+
 
 router.delete('/:id', (req, res) => {
   Comment.destroy({
